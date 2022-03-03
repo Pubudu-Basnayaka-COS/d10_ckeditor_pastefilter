7a9869741845a542dad5704fe398a48b0e482ffd
tfranz <tfranz@395239.no-reply.drupal.org>
2022-03-04 01:41:47 +0200

    Issue #3264818 by tfranz: Extend the plugin to remove even more

--- js/plugins/paste-filter/plugin.js
+++ js/plugins/paste-filter/plugin.js
@@ -51,6 +51,15 @@
         // html = html.replace( /href="http[s]*:\/\/(www\.|)example\.com\//gimsu, 'href="/');
         // remove styles from tags
         html = html.replace( /(<[^>]*) (style="[^"]*")/gimsu, '$1');
+        // remove face from tags
+        html = html.replace( /(<[^>]*) (face="[^"]*")/gimsu, '$1');
+        // remove class from tags
+        html = html.replace( /(<[^>]*) (class="[^"]*")/gimsu, '$1');
+        // remove valign from tags
+        html = html.replace( /(<[^>]*) (valign="[^"]*")/gimsu, '$1');
+        // remove font
+        html = html.replace( /<font[^>]*>/gimsu, '');
+        html = html.replace( /<\/font>/gimsu, '');
         // remove spans
         html = html.replace( /<span[^>]*>/gimsu, '');
         html = html.replace( /<\/span>/gimsu, '');
@@ -59,6 +68,8 @@
         html = html.replace( /<p><\/p>/gimsu, '');
         // remove empty <b> tag
         html = html.replace( /<b><\/b>/gimsu, '');
+        // remove empty <i> tag
+        html = html.replace( /<i><\/i>/gimsu, '');
 
         if (e.data.html)
           e.data.html = html;
